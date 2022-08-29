55a281aacb84da03ebbace789234ad5221e8fe92
Brayan Vera <BrayanVera@ravn.co>
2022-08-29 00:09:51 -0500

    feat: add react router logic to main pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./components/Login.component";
+import { ProtectedOutlet } from "./components/ProtectedOutlet";
+import { TopBar } from "./components/TopBar.component";
+import { PATHNAME } from "./utils/constants";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path={PATHNAME.LOGIN} element={<Login />} />
+      <Route path="/" element={<TopBar />}>
+        <Route path={PATHNAME.HOME} element={<TopBar />} />
+        {/* Protected Routes Below */}
+        <Route element={<ProtectedOutlet />}>
+          <Route path={PATHNAME.WATCH_LIST} element={<TopBar />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
