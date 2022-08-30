10d35693d82bd17ba41c3a1b6693fb549b22d028
Brayan Vera <BrayanVera@ravn.co>
2022-08-29 20:18:07 -0500

    refactor: change protected routes to be sibling with public routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import { Login } from "./components/Login.component";
+import { LoginForm } from "./components/LoginForm.component";
 import { ProtectedOutlet } from "./components/ProtectedOutlet";
 import { TopBar } from "./components/TopBar.component";
 import { PATHNAME } from "./utils/constants";
@@ -8,13 +8,13 @@ import { PATHNAME } from "./utils/constants";
 function App() {
   return (
     <Routes>
-      <Route path={PATHNAME.LOGIN} element={<Login />} />
+      <Route path={PATHNAME.LOGIN} element={<LoginForm />} />
       <Route path="/" element={<TopBar />}>
         <Route path={PATHNAME.HOME} element={<TopBar />} />
-        {/* Protected Routes Below */}
-        <Route element={<ProtectedOutlet />}>
-          <Route path={PATHNAME.WATCH_LIST} element={<TopBar />} />
-        </Route>
+      </Route>
+      {/* Protected Routes Below */}
+      <Route element={<ProtectedOutlet />}>
+        <Route path={PATHNAME.WATCH_LIST} element={<TopBar />} />
       </Route>
     </Routes>
   );
