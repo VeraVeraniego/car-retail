4d961c564ea3eb5e17e84381629e04b333c802c9
Brayan Vera <brayanvera@ravn.co>
2022-08-31 13:24:18 -0500

    feat: change routes

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { TopBar } from "../components/TopBar.component";
+import { Home } from "../pages/Home.page";
 import { Login } from "../pages/Login.page";
+import { PublishCar } from "../pages/PublishCar.page";
 import { PATHNAME } from "../utils/constants";
 import { ProtectedOutlet } from "./ProtectedOutlet";
 
@@ -10,12 +12,16 @@ export function AppRouter() {
     <Routes>
       <Route path={PATHNAME.LOGIN} element={<Login />} />
       <Route path="/" element={<TopBar />}>
-        <Route path={PATHNAME.HOME} element={<TopBar />} />
+        <Route path={PATHNAME.HOME} element={<Home />} />
+        <Route path={PATHNAME.PUBLISH_FORM} element={<PublishCar />} />
+        <Route element={<ProtectedOutlet />}>
+          <Route
+            path={PATHNAME.WATCH_LIST}
+            element={<Home />} /* TODO: PAGE  */
+          />
+        </Route>
       </Route>
       {/* Protected Routes Below */}
-      <Route element={<ProtectedOutlet />}>
-        <Route path={PATHNAME.WATCH_LIST} element={<TopBar />} />
-      </Route>
     </Routes>
   );
 }
