09cc05b436ef24192b7df62e670b69b61e1969c3
Brayan Vera <brayanvera@ravn.co>
2022-08-31 13:18:57 -0500

    feat: add new pages for home and publish car

---
+++ src/pages/PublishCar.page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { PublishCarForm } from "../components/PublishCarForm";
+
+export const PublishCar = () => {
+  return (
+    <div>
+      <PublishCarForm />
+    </div>
+  );
+};
