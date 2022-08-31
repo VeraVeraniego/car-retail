092363be47a28fc3ef4111ff82529d0fd4cb8765
Brayan Vera <brayanvera@ravn.co>
2022-08-30 21:51:15 -0500

    feat: add apollo graphql to index

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_API_URL,
+  cache: new InMemoryCache(),
+  headers: {
+    // "x-hasura-admin-secret": `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+    "x-hasura-admin-secret": import.meta.env.VITE_API_TOKEN,
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </ApolloProvider>
 );
