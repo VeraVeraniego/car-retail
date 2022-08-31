d94da5f24651b59ed68921d2d745633a6aeb0c2b
Brayan Vera <brayanvera@ravn.co>
2022-08-31 18:49:20 -0500

    feat: add context provider to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { UserContext } from "./context/UserContext";
+import { User } from "./interfaces/User";
 import { AppRouter } from "./routes/AppRouter";
 
 const App = () => {
-  return <AppRouter />;
+  const [user, setUser] = useState<User | undefined>();
+  //TODO: get from local storage and put insedo provider
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <AppRouter />;
+    </UserContext.Provider>
+  );
 };
 
 export default App;
