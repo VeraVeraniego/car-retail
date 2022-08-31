a71c31d6e6a64ba45b8632d486d8dc2f227cdfb4
Brayan Vera <brayanvera@ravn.co>
2022-08-31 12:12:23 -0500

    rebase into last commit

--- src/components/ProtectedOutlet.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate, Outlet, Route, RouteProps } from "react-router-dom";
-
-export const ProtectedOutlet = (props: any) => {
-  const isLoggedIn: boolean = true;
-  return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
-  // return <Route {...props} />;
-};
