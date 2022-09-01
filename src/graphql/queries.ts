072aed7eb9065befd6668e15ddc11f16ac1a1f04
Brayan Vera <BrayanVera@ravn.co>
2022-09-01 13:08:24 -0500

    feat: add cars query

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -7,7 +7,43 @@ export const VALIDATE_EMAIL = gql`
       first_name
       id
       last_name
-      uuid
+    }
+  }
+`;
+export const GET_CARS = gql`
+  query Cars {
+    cars {
+      batch
+      city {
+        name
+        id
+        state {
+          id
+          name
+        }
+      }
+      color {
+        id
+        name
+      }
+      condition
+      damage_type
+      description
+      id
+      odometer
+      price
+      sale_date
+      title
+      vin
+      year
+      model {
+        id
+        name
+        brand {
+          id
+          name
+        }
+      }
     }
   }
 `;
