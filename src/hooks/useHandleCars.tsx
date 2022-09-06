9af3df683cd670eebf3064e9fc4a825cb1ebf626
Brayan Vera <BrayanVera@ravn.co>
2022-09-06 11:56:14 -0500

    fix: change handle cars custom hook to order by quering backend

--- src/hooks/useHandleCars.tsx
+++ src/hooks/useHandleCars.tsx
@@ -1,10 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-  Cars,
-  useCarsQuery,
-  useOrderedCarsLazyQuery,
-  useOrderedCarsQuery,
-} from "../graphql/generated/graphql";
+import { Cars, Order_By, useCarsQuery } from "../graphql/generated/graphql";
 import { CarRowInfo, Filters, SortOrder } from "../interfaces/Car";
 import { adaptResponse } from "../utils/CarAdapter.util";
 
@@ -15,8 +10,8 @@ export const useHandleCars = () => {
   //   error: errorOnInit,
   //   loading: loadingOnInit,
   // } = useCarsQuery();
-  const [orderBy, setOrderBy] = useState<SortOrder>("DESC");
-  const { data, error, loading, refetch } = useOrderedCarsQuery();
+  const [orderBy, setOrderBy] = useState<Order_By | "">(Order_By.Desc);
+  const { data, error, loading, refetch } = useCarsQuery();
 
   useEffect(() => {
     if (!data) return;
@@ -24,28 +19,32 @@ export const useHandleCars = () => {
     setCars(adaptedCars);
   }, [data]);
 
-  function toogleOrder() {
+  async function toogleOrder() {
     if (!cars) return "";
-    if (orderBy === "ASC") {
-      setOrderBy("DESC");
-      const carsSortedAsc = cars?.sort((a, b) => {
-        return new Date(a.saleDate).valueOf() - new Date(b.saleDate).valueOf();
-      });
-      setCars(carsSortedAsc);
+    let order: Order_By;
+    if (orderBy === Order_By.Asc) {
+      setOrderBy(Order_By.Desc);
+      order = Order_By.Desc;
     } else {
-      // FIXME: bug on states when initialized on ""
-      if (orderBy === "DESC" || orderBy === "") {
-        setOrderBy("ASC");
-        const carsSortedDesc = cars?.sort((a, b) => {
-          return (
-            new Date(b.saleDate).valueOf() - new Date(a.saleDate).valueOf()
-          );
-        });
-        setCars(carsSortedDesc);
+      // FIXME: bug on orderBy state when initialized on ""
+      if (orderBy === Order_By.Desc || orderBy === "") {
+        console.log("is empty string", orderBy === "");
+
+        setOrderBy(Order_By.Asc);
+        order = Order_By.Asc;
       }
     }
+    const response = await refetch({
+      orderBy: [
+        {
+          sale_date: order!,
+        },
+      ],
+    });
+    const carsSorted = adaptResponse(response.data.cars as Cars[]);
+    setCars(carsSorted);
+    console.log("order by ", orderBy);
 
-    console.log("end", orderBy);
     return orderBy;
   }
 
@@ -65,3 +64,28 @@ export const useHandleCars = () => {
 
   return { data: { cars, error, loading }, toogleOrder, searchInInventory };
 };
+
+// async function toogleOrder() {
+//   if (!cars) return "";
+//   if (orderBy === "ASC") {
+//     setOrderBy("DESC");
+//     const carsSortedAsc = cars?.sort((a, b) => {
+//       return new Date(a.saleDate).valueOf() - new Date(b.saleDate).valueOf();
+//     });
+//     setCars(carsSortedAsc);
+//   } else {
+//     // FIXME: bug on orderBy state when initialized on ""
+//     if (orderBy === "DESC" || orderBy === "") {
+//       setOrderBy("ASC");
+//       const carsSortedDesc = cars?.sort((a, b) => {
+//         return (
+//           new Date(b.saleDate).valueOf() - new Date(a.saleDate).valueOf()
+//         );
+//       });
+//       setCars(carsSortedDesc);
+//     }
+//   }
+
+//   console.log("end", orderBy);
+//   return orderBy;
+// }
