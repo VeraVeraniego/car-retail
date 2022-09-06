adee32a3b9b42b7cd092d180869257a9c70c59ea
Brayan Vera <BrayanVera@ravn.co>
2022-09-06 18:51:11 -0500

    feat: add variables for query filtering

---
+++ src/graphql/variables.tsx
@@ -0,0 +1,39 @@
+import { Order_By } from "./generated/graphql";
+
+export function orderVariables(order: Order_By) {
+  return {
+    orderBy: [
+      {
+        sale_date: order,
+      },
+    ],
+  };
+}
+
+export function searchByTitleVariables(title: string) {
+  return {
+    where: {
+      title: {
+        _ilike: `%${title}%`,
+      },
+    },
+  };
+}
+export function searchByVINVariables(vin: string) {
+  return {
+    where: {
+      vin: {
+        _eq: vin,
+      },
+    },
+  };
+}
+export function searchByBatchVariables(batch: string) {
+  return {
+    where: {
+      batch: {
+        _eq: batch,
+      },
+    },
+  };
+}
