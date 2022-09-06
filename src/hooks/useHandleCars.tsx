5102568e24ab59eaf96bec83f03ea3a4e08346da
Brayan Vera <BrayanVera@ravn.co>
2022-09-05 23:32:08 -0500

    feat: add orderBy functionality to custom hook

--- src/hooks/useHandleCars.tsx
+++ src/hooks/useHandleCars.tsx
@@ -1,10 +1,49 @@
-import React, { useState } from "react";
-import { useOrderedCarsLazyQuery } from "../graphql/generated/graphql";
+import React, { useEffect, useState } from "react";
+import {
+  Cars,
+  useCarsQuery,
+  useOrderedCarsLazyQuery,
+  useOrderedCarsQuery,
+} from "../graphql/generated/graphql";
+import { CarRowInfo, Filters } from "../interfaces/Car";
+import { adaptResponse } from "../utils/CarAdapter.util";
 
+type SortOrder = "" | "ASC" | "DESC";
 export const useHandleCars = () => {
-  const [getCars, { data, error, loading }] = useOrderedCarsLazyQuery();
+  const [cars, setCars] = useState<CarRowInfo[] | null>(null);
+  // const {
+  //   data: dataOnInit,
+  //   error: errorOnInit,
+  //   loading: loadingOnInit,
+  // } = useCarsQuery();
+  const [orderBy, setOrderBy] = useState<SortOrder>("");
+  const [searchText, setSearchText] = useState<string>("");
+  const { data, error, loading, refetch } = useOrderedCarsQuery();
 
-  const [cars, setCars] = useState(data);
+  useEffect(() => {
+    if (!data) return;
+    const adaptedCars = adaptResponse(data.cars as Cars[]);
+    setCars(adaptedCars);
+  }, [data]);
 
-  return { cars };
+  function toogleOrder() {
+    if (!cars) return console.error("Cars not available");
+    if (orderBy === "ASC") {
+      const carsSortedAsc = cars?.sort((a, b) => {
+        return new Date(b.saleDate).valueOf() - new Date(a.saleDate).valueOf();
+      });
+      setCars(carsSortedAsc);
+      setOrderBy("DESC");
+    } else if (orderBy === "DESC" || orderBy === "") {
+      const carsSortedDesc = cars?.sort((a, b) => {
+        return new Date(a.saleDate).valueOf() - new Date(b.saleDate).valueOf();
+      });
+
+      setCars(carsSortedDesc);
+      setOrderBy("ASC");
+    }
+    return orderBy;
+  }
+
+  return { cars, toogleOrder };
 };
