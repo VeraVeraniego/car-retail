f62a57c01b21b32a04baeff4d32394448fa2480d
Brayan Vera <BrayanVera@ravn.co>
2022-09-22 23:55:08 -0500

    feat: add tests to login page

--- src/components/LoginForm.test.tsx
+++ src/components/LoginForm.test.tsx
@@ -1,34 +1,93 @@
 import { MockedProvider } from "@apollo/client/testing";
-import "@testing-library/jest-dom";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import {
+  findByDisplayValue,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { BrowserRouter } from "react-router-dom";
-import { describe, expect, test } from "vitest";
+// import * as router from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 
-import { Login } from "./../pages/Login/index";
+import {
+  acceptedEmail,
+  rejectedEmail,
+  validationMock,
+} from "../test/user-mocks";
+import { LoginForm } from "./../components/LoginForm.component";
 
-beforeEach(() => {
-  render(
-    <BrowserRouter>
-      <MockedProvider>
-        <Login />
-      </MockedProvider>
-    </BrowserRouter>
-  );
+const user = userEvent.setup();
+
+describe("LoginForm basic rendering", () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <MockedProvider>
+          <LoginForm />
+        </MockedProvider>
+      </MemoryRouter>
+    );
+  });
+
+  test("Welcome is rendered", () => {
+    expect(screen.getByText(/welcome!/i)).toBeInTheDocument();
+  });
+
+  test("button enables on input change", async () => {
+    const email = screen.getByPlaceholderText(/email/i);
+    const loginButton = screen.getByRole("button", { name: /login/i });
+    expect(loginButton).toBeDisabled();
+    await user.type(email, rejectedEmail);
+    expect(loginButton).toBeEnabled();
+  });
+
+  test("should show invalid email when applies", async () => {
+    const email = screen.getByPlaceholderText(/email/i);
+    const loginButton = screen.getByRole("button", { name: /login/i });
+    await user.type(email, "test");
+    await user.click(loginButton);
+    expect(screen.getByText(/invalid email/i)).toBeInTheDocument();
+  });
 });
 
-test("Welcome is rendered", () => {
-  expect(screen.getByText(/welcome!/i)).toBeInTheDocument();
-});
+describe("LoginForm connection to API", () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <MockedProvider mocks={validationMock} addTypename={false}>
+          <LoginForm />
+        </MockedProvider>
+      </MemoryRouter>
+    );
+  });
+  test("should show validation text when not allowed to login", async () => {
+    const email = screen.getByPlaceholderText(/email/i);
+    const loginButton = screen.getByRole("button", { name: /login/i });
+    await user.type(email, rejectedEmail);
+    await user.click(loginButton);
+    expect(screen.getByText(/not allowed/i)).toBeInTheDocument();
+  });
+  // test("should navigate to home when user allowed", async () => {
+  //   // const navigate = vi.fn();
+  //   // const spy = vi
+  //   //   .spyOn(ruter, "useNavigate")
+  //   //   .mockImplementation(() => navigate);
+  //   // ///////
+  //   // vi.mock("react-router-dom");
+  //   // (router as any).useNavigate = vi.fn();
+  //   // });
+  //   // const navigate = useNavigate();
+  //   // const spy = vi.spyOn(router, "useNavigate");
+  //   // ////
+  //   const email = screen.getByPlaceholderText(/email/i);
+  //   const loginButton = screen.getByRole("button", { name: /login/i });
+  //   await user.type(email, acceptedEmail);
+  //   await user.click(loginButton);
 
-test("email input is in the document", () => {
-  expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
-});
-
-test("email input change when user input", async () => {
-  const user = userEvent.setup();
-  const email = screen.getByPlaceholderText(/email/i);
-  await user.type(email, "test");
-
-  waitFor(() => expect(email).toHaveTextContent("test"));
+  //   await waitFor(async () =>
+  //     expect(await screen.findByPlaceholderText(/email/i)).toHaveTextContent(
+  //       /redirecting/i
+  //     )
+  //   );
+  // });
 });
